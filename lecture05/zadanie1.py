05843f9d9b857c1c1db2bc0017c842697e09206d
Владимир Пасько <90717196+VGENDA@users.noreply.github.com>
2021-10-22 23:26:17 +0700

    Add files via upload

---
+++ lecture05/zadanie1.py
@@ -0,0 +1,26 @@
+import random as r
+
+file = open("task1.txt", 'rt')
+
+a = []
+
+for line in file:
+    a.append(int(line))
+
+print(a)
+
+sum = 0
+while sum != 2020:
+    s1 = a[r.randint(0, 199)]
+    s2 = a[r.randint(0, 199)]
+    s3 = a[r.randint(0, 199)]
+    sum = s1 + s2 + s3
+    rez = s1 * s2 * s3
+
+print(s1, '+', s2, '+', s3, '=', sum)
+print(s1, '*', s2, '*', s3, '=', rez)
+
+f = open('output1.txt', 'w')
+f.write(str(rez))
+print("Результат записан")
+f.close()
